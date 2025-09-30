b791337348ec79681d54e719fe2fb597f206483f
jjorgeb <jorge.barraza@uchile.cl>
2025-09-30 16:16:57 -0300

    chore: init project

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="eol_sso",
+    version="0.0.1",
+    author="EOL Uchile",
+    author_email="eol-ing@uchile.cl",
+    description="Middleware between apps and uchileedxlogin/eol_sso_login",
+    long_description="Middleware between apps and uchileedxlogin/eol_sso_login",
+    url="https://github/eol-uchile/eol_sso",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "lms.djangoapp": ["eol_sso = eol_sso.apps:EolSsoConfig"],
+        "cms.djangoapp": ["eol_sso = eol_sso.apps:EolSsoConfig"]
+    },
+)
