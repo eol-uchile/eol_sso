c75d2fbfaa1c775a166a6609103f7f4637994c37
jjorgeb <jorge.barraza@uchile.cl>
2025-10-02 16:59:46 -0300

    chore: updates version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="eol_sso",
-    version="0.0.1",
+    version="1.0.0",
     author="EOL Uchile",
     author_email="eol-ing@uchile.cl",
     description="Middleware between apps and uchileedxlogin/eol_sso_login",
