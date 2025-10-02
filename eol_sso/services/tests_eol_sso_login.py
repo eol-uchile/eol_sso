c0d582ad22ffc4d9c9c5580179fa7045f2a44b52
jjorgeb <jorge.barraza@uchile.cl>
2025-10-02 16:59:34 -0300

    test: update tests to use new changes on the interface and clean up descriptions

--- eol_sso/services/tests_eol_sso_login.py
+++ eol_sso/services/tests_eol_sso_login.py
@@ -3,15 +3,16 @@
 import logging
 
 # Installed packages (via pip)
+from django.contrib.auth.models import User
 from django.test import TestCase
-from mock import Mock, patch
+from mock import patch
 from eol_sso_login.models import SSOLoginExtraData
 
 # Edx dependencies
 from student.tests.factories import UserFactory
 
 # Internal project dependencies
-from eol_sso.services.interface import get_document_type, get_doc_id_by_user_id, get_user_id_doc_id_pairs
+from eol_sso.services.interface import get_indiv_id, get_user_by_indiv_id, get_user_id_with_indiv_id_list
 
 logger = logging.getLogger(__name__)
 
@@ -23,60 +24,66 @@ class EolSsoInterfaceTests(TestCase):
         # to create a new cc user when creating a django user
         with patch('student.models.cc.User.save'):
             self.user = UserFactory(username='student', email='student@edx.org')
-            self.user2 = UserFactory(username='student2', email='student2@edx.org')
 
     @patch('eol_sso.services.interface.SSOLoginExtraData.objects')
-    def test_get_doc_id_by_user_id_none_return_value(self, mock_get):
+    def test_get_indiv_id_none_return_value(self, mock_get):
         """
-        Test get_doc_id_by_user_id when the user doesn't have extra data
+        Test get_indiv_id when the user doesn't have extra data
         """
         mock_get.values_list.return_value.get.side_effect = SSOLoginExtraData.DoesNotExist
-        doc_id = get_doc_id_by_user_id(self.user.id)
-        self.assertEqual(doc_id, None)
+        indiv_id = get_indiv_id(self.user.id)
+        self.assertEqual(indiv_id, None)
 
     @patch('eol_sso.services.interface.SSOLoginExtraData.objects')
-    def test_get_doc_id_by_user_id_doc_id_return_value(self, mock_get):
+    def test_get_indiv_id_rut(self, mock_get):
         """
-        Test get_doc_id_by_user_id when the user has extra data
+        Test get_indiv_id when the user has a indiv_id
         """
-        mock_get.values_list.return_value.get.return_value = 12345678
-        doc_id = get_doc_id_by_user_id(self.user.id)
-        self.assertEqual(doc_id, 12345678)
+        mock_get.values_list.return_value.get.return_value = '12345678'
+        indiv_id = get_indiv_id(self.user.id)
+        self.assertEqual(indiv_id, '12345678')
 
     @patch('eol_sso.services.interface.SSOLoginExtraData.objects')
-    def test_get_user_id_doc_id_pairs_empty_list_return_value(self, mock_user_doc_id_pair):
+    def test_get_indiv_id_passport(self, mock_get):
         """
-        Test get_user_id_doc_id_pairs when the user doesn't have extra data
+        Test get_indiv_id when the user has a indiv_id following the passport format
         """
-        mock_user_doc_id_pair.filter.return_value.values_list.return_value = []
-        user_id_doc_id_list = get_user_id_doc_id_pairs([self.user.id])
-        self.assertEqual(user_id_doc_id_list, [])
+        mock_get.values_list.return_value.get.return_value = 'P2345678'
+        indiv_id = get_indiv_id(self.user.id)
+        self.assertEqual(indiv_id, 'P2345678')
 
     @patch('eol_sso.services.interface.SSOLoginExtraData.objects')
-    def test_get_user_id_doc_id_pairs_pair_return_value(self, mock_user_doc_id_pair):
+    def test_get_user_id_with_indiv_id_list_empty_list_return_value(self, mock_user_doc_id_queryset):
         """
-        Test get_user_id_doc_id_pairs when the user has extra data
+        Test get_user_id_with_indiv_id_list when the user doesn't have extra data
         """
-        mock_user_doc_id_pair.filter.return_value.values_list.return_value = [(self.user.id, 1234568)]
-        user_id_doc_id_list = get_user_id_doc_id_pairs([self.user.id])
-        self.assertEqual(user_id_doc_id_list, [(self.user.id, 1234568)])
+        mock_user_doc_id_queryset.filter.return_value.values_list.return_value = []
+        user_id_doc_tuples_list = get_user_id_with_indiv_id_list([self.user.id])
+        self.assertEqual(user_id_doc_tuples_list, [])
 
     @patch('eol_sso.services.interface.SSOLoginExtraData.objects')
-    def test_get_document_type_extra_data(self, mock_get):
+    def test_get_user_id_with_indiv_id_list(self, mock_user_doc_id_queryset):
         """
-        Test get_document_type when the user has extra data
+        Test get_user_id_with_indiv_id_list when the user has extra data
         """
-        mock_query_response = Mock()
-        mock_query_response.type_document = 'run'
-        mock_get.get.return_value = mock_query_response
-        document_type = get_document_type(12346787)
-        self.assertEqual(document_type, 'run')
+        mock_user_doc_id_queryset.filter.return_value.values_list.return_value = [(self.user.id, '1234568')]
+        user_id_doc_tuples_list = get_user_id_with_indiv_id_list([self.user.id])
+        self.assertEqual(user_id_doc_tuples_list, [(self.user.id, '1234568')])
 
-    @patch('eol_sso.services.interface.SSOLoginExtraData.objects')
-    def test_get_document_type_none_return_value(self, mock_get):
+    @patch('eol_sso.services.interface.User.objects.get')
+    def test_get_user_by_indiv_id_none_return_value(self, mock_user):
         """
-        Test get_doc_id_by_user_id when the user doesn't have extra data
+        Test get_user_by_indiv_id when the indiv_id is not linked to any users
         """
-        mock_get.get.side_effect = SSOLoginExtraData.DoesNotExist
-        document_type = get_document_type(12346787)
-        self.assertEqual(document_type, None)
+        mock_user.side_effect = User.DoesNotExist
+        user = get_user_by_indiv_id('123456789')
+        self.assertEqual(user, None)
+
+    @patch('eol_sso.services.interface.User.objects.get')
+    def test_get_user_by_indiv_id_user_return_value(self, mock_response):
+        """
+        Test get_user_by_indiv_id when the indiv_id is linked to a user
+        """
+        mock_response.return_value = self.user
+        user = get_user_by_indiv_id('P123456789')
+        self.assertEqual(user, self.user)
