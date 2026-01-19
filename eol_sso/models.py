e52e23786ce5d3cb3f25f0c672db2ac932e36caf
jjorgeb <jorge.barraza@uchile.cl>
2026-01-19 17:43:44 -0300

    feat: add UserSSo model, initial migration and admin registration

--- eol_sso/models.py
+++ eol_sso/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserSso(models.Model):
+    indiv_id = models.CharField(max_length=20, unique=True, db_index=True)
+    id_persona = models.IntegerField(unique=True, db_index=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True, db_index=True)
