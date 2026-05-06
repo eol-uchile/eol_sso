91796d4493f3fdcfba5ea32484ca4bef52846c3d
fsologureng <fsologur@uchile.cl>
2026-05-06 18:19:19 -0400

    ci: fix custom pythonapp wf
    - Restore & refactor old feedback custom workflow.
    - Remove ci exclution paths.
    - Change branch matches.

--- .github/test-eol.sh
+++ .github/test-eol.sh
@@ -8,20 +8,9 @@ pip install --src /openedx/venv/src -e /openedx/requirements/app
 cd /openedx/requirements/app
 
 # test uchileedxlogin
-echo 'TESTING WITH uchileedxlogin'
 pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/uchileedxlogin@2.0.0#egg=uchileedxlogin
 mkdir -p test_root
 ln -s /openedx/staticfiles ./test_root/
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest -s -vvv --no-cov eol_sso/services/tests_uchileedxlogin.py
-
-rm -rf test_root
-rm -rf /openedx/venv/src/uchileedxlogin
-
-# test eol_sso_login
-echo 'TESTING WITH eol_sso_login'
-pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/eol_sso_login@0.1.1#egg=eol_sso_login
-mkdir -p test_root
-ln -s /openedx/staticfiles ./test_root/
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest -s -vvv --no-cov eol_sso/services/tests_eol_sso_login.py
 
 rm -rf test_root .coverage .coverage.xml
