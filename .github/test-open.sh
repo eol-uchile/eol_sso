c9616ad3cadd89120eb4c87617917898cb57e75e
jjorgeb <jorge.barraza@uchile.cl>
2026-06-30 15:51:19 -0400

    ci: add testing for tests.py

--- .github/test-open.sh
+++ .github/test-open.sh
@@ -11,6 +11,6 @@ cd /openedx/requirements/app
 pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/eol_sso_login@0.1.1#egg=eol_sso_login
 mkdir -p test_root
 ln -s /openedx/staticfiles ./test_root/
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest -s -vvv --no-cov eol_sso/services/tests_eol_sso_login.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest -s -vvv --no-cov eol_sso/services/tests_eol_sso_login.py eol_sso/tests.py
 
 rm -rf test_root .coverage .coverage.xml
