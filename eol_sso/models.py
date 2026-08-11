e00d2f0394a1a5b9e55015bc32254a65454ee90b
jjorgeb <jorge.barraza@uchile.cl>
2026-08-11 17:07:05 -0400

    chore!: remove unused UserSso migration tooling and model

--- eol_sso/models.py
+++ eol_sso/models.py
@@ -1,15 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class UserSso(models.Model):
-    indiv_id = models.CharField(max_length=20, unique=True, db_index=True)
-    id_persona = models.IntegerField(unique=True, db_index=True, null=True, blank=True)
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True, db_index=True)
-
 class UserIndivId(models.Model):
     indiv_id = models.CharField(max_length=20, unique=True, db_index=True)
     user = models.OneToOneField(
